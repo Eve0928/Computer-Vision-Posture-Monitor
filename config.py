e3efa9942638f74a951613ddb33f9258a5d36daa
Evelyn Choi <evlyn.c28@gmail.com>
2026-09-16 12:07:47 -0400

    update slouch tolerance for more accuracy in good poster

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 MODEL_PATH = "yolov8n-pose.pt"
 SMOOTHING_FRAMES = 15
 #if slouch alert triggers too easily, lower the tolerance
-SLOUCH_TOLERANCE = 0.80
+SLOUCH_TOLERANCE = 0.90
 
 
 # YOLO Pose Keypoint Indices
@@ -11,4 +11,5 @@ L_EAR = 3
 R_EAR = 4
 L_SHOULDER = 5
 R_SHOULDER = 6
+
 
