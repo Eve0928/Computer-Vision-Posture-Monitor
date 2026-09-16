0f725b3ba971674bb284fe25f4cbe4ffe7ee4a8d
Evelyn Choi <evlyn.c28@gmail.com>
2026-09-16 10:19:53 -0400

    config.py ; control panel, set specific indices for YOLO

---
+++ config.py
@@ -0,0 +1,14 @@
+# Model and logic settings
+MODEL_PATH = "yolov8n-pose.pt"
+SMOOTHING_FRAMES = 15
+#if slouch alert triggers too easily, lower the tolerance
+SLOUCH_TOLERANCE = 0.80
+
+
+# YOLO Pose Keypoint Indices
+NOSE = 0
+L_EAR = 3
+R_EAR = 4
+L_SHOULDER = 5
+R_SHOULDER = 6
+
